81f9a4463fa7fa1c4fff63ac27b4137d389b1cca
Elise Frigoli <elise.frigoli@gmail.com>
2021-11-26 13:32:47 -0600

    Linking to property table

---
+++ includes/property_table.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+include  "Inc.DBC.php";
+if (isset($_SESSION['username']) && isset($_SESSION['admin_id'])) {
+
+    $sql = "SELECT * FROM property ORDER BY property_id ASC";
+    $res = mysqli_query($conn, $sql);
+} else {
+    header("Location: index.php");
+}
