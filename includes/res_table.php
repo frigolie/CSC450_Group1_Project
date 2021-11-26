8303badbecab943ece75ce535d101142238f8929
Elise Frigoli <elise.frigoli@gmail.com>
2021-11-26 13:31:43 -0600

    Linking to reservation table

---
+++ includes/res_table.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+include  "Inc.DBC.php";
+if (isset($_SESSION['username']) && isset($_SESSION['admin_id'])) {
+
+    $sql = "SELECT * FROM reservation ORDER BY id ASC";
+    $res = mysqli_query($conn, $sql);
+} else {
+    header("Location: index.php");
+}
