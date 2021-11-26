d94fae67e6e455803b4c5851dacfbf215e4925e4
Elise Frigoli <elise.frigoli@gmail.com>
2021-11-26 13:33:59 -0600

    Updates to admin dashboard
    Added "Edit profile" link to admin account card. Added tables for user data, property data, and reservation data (conditionally displaying based on whether there are users/properties/reservations in the table). Styling changes.

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['admin_id'])) {   ?>
     <html>
 
     <head>
-        <title>HOME</title>
+        <title>Admin Dashboard</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
@@ -17,24 +17,27 @@ if (isset($_SESSION['username']) && isset($_SESSION['admin_id'])) {   ?>
         <?php include(getcwd() . "/header.php"); ?>
         <section class="container-fluid initialPageContent greenMountains pb-5">
 
-            <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh">
+            <div class="container" style="min-height: 100vh">
                 <?php if ($_SESSION['role'] == 'admin') { ?>
+                  <div class="row w-100 d-flex flex-wrap justify-content-center align-items-start">
                     <!--Admin Users-->
-                    <div class="card" style="width: 18rem;">
+                    <div class="col-3 adminCard py-4 position-sticky white-bg rounded-custom box-shadow">
                         <img src="graphic/admin.png" class="card-img-top" alt="admin image">
                         <div class="card-body text-center">
-                            <h5 class="card-title">
+                            <h5 class="card-title mb-4">
                                 <?= $_SESSION['name'] ?>
                             </h5>
-                            <a href="logout.php" class="globalButton blueButton w-100">Logout</a>
+                            <a href="/edit-account.php" class="globalButton blueButton w-100 mt-5">Edit Account</a>
+                            <a href="/logout.php" class="globalButton redButton w-100 mt-5">Logout</a>
                         </div>
                     </div>
-                    <div class="p-3">
-                        <?php include 'includes/admin_user.php';
-                        if (mysqli_num_rows($res) > 0) { ?>
 
-                            <h1 class="display-4 fs-1">Admins</h1>
-                            <table class="table" style="width: 32rem;">
+                    <!-- Admin info table -->
+                    <?php include 'includes/admin_user.php';
+                      if (mysqli_num_rows($res) > 0) { ?>
+                        <div class="col-8 offset-1 p-4 mb-0 rounded-custom white-bg box-shadow adminDashTable" id="adminTable">
+                            <h1 class="fs-1 mb-3 text-center text-shadow">Admins</h1>
+                            <table class="table">
                                 <thead>
                                     <tr>
                                         <th scope="col">#</th>
@@ -57,15 +60,113 @@ if (isset($_SESSION['username']) && isset($_SESSION['admin_id'])) {   ?>
                                     } ?>
                                 </tbody>
                             </table>
+                          </div>
                         <?php } ?>
-                    </div>
-                <?php } else { ?>
+                    <!-- User info table -->
+                    <?php include 'includes/user_table.php';
+                      if (mysqli_num_rows($res) > 0) { ?>
+                        <div class="col-8 offset-4 p-4 mb-5 rounded-custom white-bg box-shadow adminDashTable" id="userTable">
+                            <h1 class="fs-1 mb-3 text-center text-shadow">Users</h1>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First Name</th>
+                                        <th scope="col">Last Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Username</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $i = 1;
+                                    while ($rows = mysqli_fetch_assoc($res)) { ?>
+                                        <tr>
+                                            <th scope="row"><?= $i ?></th>
+                                            <td><?= $rows['fname'] ?></td>
+                                            <td><?= $rows['lname'] ?></td>
+                                            <td><?= $rows['email'] ?></td>
+                                            <td><?= $rows['username'] ?></td>
+                                        </tr>
+                                    <?php $i++;
+                                    } ?>
+                                </tbody>
+                            </table>
+                          </div>
+                        <?php } ?>
+                    <!-- Property info table -->
+                    <?php include 'includes/user_table.php';
+                      if (mysqli_num_rows($res) > 0) { ?>
+                        <div class="col-8 offset-4 p-4 mb-5 mt-4 rounded-custom white-bg box-shadow adminDashTable" id="userTable">
+                            <h1 class="fs-1 mb-3 text-center text-shadow">Properties</h1>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Address</th>
+                                        <th scope="col">Owner</th>
+                                        <th scope="col">Bedrooms</th>
+                                        <th scope="col">Bathrooms</th>
+                                        <th scope="col">Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $i = 1;
+                                    while ($rows = mysqli_fetch_assoc($res)) { ?>
+                                        <tr>
+                                            <th scope="row"><?= $i ?></th>
+                                            <td><?= $rows['fname'] ?></td>
+                                            <td><?= $rows['lname'] ?></td>
+                                            <td><?= $rows['email'] ?></td>
+                                            <td><?= $rows['username'] ?></td>
+                                        </tr>
+                                    <?php $i++;
+                                    } ?>
+                                </tbody>
+                            </table>
+                          </div>
+                        <?php } ?>
+                    <!-- Reservation info table -->
+                    <?php include 'includes/res_table.php';
+                      if (mysqli_num_rows($res) > 0) { ?>
+                        <div class="col-8 offset-4 p-4 mb-5 mt-4 rounded-custom white-bg box-shadow adminDashTable" id="userTable">
 
+                            <h1 class="fs-1 mb-3 text-center text-shadow">Reservations</h1>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Property</th>
+                                        <th scope="col">Dates</th>
+                                        <th scope="col">Adults</th>
+                                        <th scope="col">Kids</th>
+                                        <th scope="col">Pets</th>
+                                        <th scope="col">Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $i = 1;
+                                    while ($rows = mysqli_fetch_assoc($res)) { ?>
+                                        <tr>
+                                            <th scope="row"><?= $i ?></th>
+                                            <td><?= $rows['fname'] ?></td>
+                                            <td><?= $rows['lname'] ?></td>
+                                            <td><?= $rows['email'] ?></td>
+                                            <td><?= $rows['username'] ?></td>
+                                        </tr>
+                                    <?php $i++;
+                                    } ?>
+                                </tbody>
+                            </table>
+                          </div>
+                        <?php } ?>
 
-                <?php } ?>
-
-
-
+                  </div>
+                <?php } else { // Display nothing
+                } ?>
             </div>
         </section>
         <?php include(getcwd() . "/footer.php"); ?>
